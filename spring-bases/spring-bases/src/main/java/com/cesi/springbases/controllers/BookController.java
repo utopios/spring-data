21b02d16c8af0a6aae402f57000490a56f23ab39
GharrowBM <gharrow.games@gmail.com>
2023-03-13 12:18:05 +0100

    Spring MVC Books done

--- spring-bases/spring-bases/src/main/java/com/cesi/springbases/controllers/BookController.java
+++ spring-bases/spring-bases/src/main/java/com/cesi/springbases/controllers/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -47,6 +48,34 @@ public class BookController {
             return "createBook";
         }
         bookService.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBookForm(@PathVariable("id") Long id, Model model) {
+        Book bookFound = bookService.findById(id);
+
+        if (bookFound != null) {
+            model.addAttribute("book", bookFound);
+            return "editBook";
+        } else {
+            return "redirect:/books";
+        }
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long id, @Validated Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "editBook";
+        }
+        book.setId(id);
+        bookService.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        bookService.deleteById(id);
         return "redirect:/books";
     }
 }
